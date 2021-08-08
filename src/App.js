875201419aa4be7201080955d434332987fb58d0
essencedev9 <essencedev9@nntuple.com>
2021-08-08 15:20:46 +0900

    ThemeProvider에 theme에서 정의한 칼라셋을 설정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
-import styled from 'styled-components/native';
+import styled, { ThemeProvider } from 'styled-components/native';
+import { theme } from './theme';
 
 const Container = styled.View`
   flex: 1;
-  background-color: #ffffff;
+  background-color: ${({ theme }) => theme.background};
   align-items: center;
   justify-content: center;
 `;
 
 export default function App() {
   return (
-    <Container>
-      <Text>TODO</Text>
-      <StatusBar style="auto" />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Text>TODO</Text>
+        <StatusBar style="auto" />
+      </Container>
+    </ThemeProvider>
   );
 }
